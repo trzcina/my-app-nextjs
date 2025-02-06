aada30970f232e997a8cc329f72392b6a455c571
Michał Chrząszcz <mchrzaszcz90@gmail.com>
2025-02-06 13:21:40 +0100

    Fix eslint

--- src/app/blogpost/page.tsx
+++ src/app/blogpost/page.tsx
@@ -4,12 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-export default async function PostPage({
-  params,
-}: {
-  readonly params: Promise<{ readonly slug: string }>;
-}) {
-  const { slug } = await params;
+export default async function PostPage() {
   const { isEnabled } = await draftMode();
   
   /**
